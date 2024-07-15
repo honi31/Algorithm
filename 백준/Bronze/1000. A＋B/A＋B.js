4830f8fc89f24e2524bd221515031365fe495b02
Honi <87221653+honi31@users.noreply.github.com>
2024-07-15 13:33:17 +0900

    [Bronze V] Title: A+B, Time: 104 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,6 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+let arr = input[0].split(" ")
+let a = parseInt(arr[0])
+let b = parseInt(arr[1])
+console.log(a+b)
