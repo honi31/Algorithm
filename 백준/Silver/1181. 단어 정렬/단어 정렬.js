dc6f58056ee70d6aaf693318862eaf8612ce0fbc
Honi <87221653+honi31@users.noreply.github.com>
2024-07-25 22:33:10 +0900

    [Silver V] Title: 단어 정렬, Time: 240 ms, Memory: 18944 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,26 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let n = Number(input[0]);
+let arr = [];
+for (let i = 1; i <= n; i++) {
+  arr.push(input[i]);
+}
+
+arr = [...new Set(arr)];
+
+function compare(a, b) {
+  if (a.length !== b.length) {
+    return a.length - b.length;
+  } else {
+    if (a < b) return -1;
+    else if (a > b) return 1;
+    else return 0;
+  }
+}
+arr.sort(compare);
+let answer = "";
+for (let el of arr) {
+  answer += el + "\n";
+}
+console.log(answer);
