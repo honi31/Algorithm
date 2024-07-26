2762447ad7a8e6747e49f56a1ca9b5c27e21b2b9
Honi <87221653+honi31@users.noreply.github.com>
2024-07-26 21:03:05 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.js
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -2,8 +2,12 @@ let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().trim();
 
 let arr = input.split("").map(Number);
+function compare(a, b) {
+  if (a < b) {
+    return 1;
+  } else return -1;
+}
 
-arr.sort((a, b) => b - a);
+arr.sort(compare);
 let answer = arr.join("");
-
 console.log(answer);
