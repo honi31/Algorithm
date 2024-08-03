0738496d4b46e1fee533beb1f8bcbc00d5ca8725
Honi <87221653+honi31@users.noreply.github.com>
2024-08-03 12:26:57 +0900

    [Silver IV] Title: 박 터뜨리기, Time: 104 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.js
@@ -0,0 +1,17 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+let n = Number(input[0]);
+let k = Number(input[1]);
+let sum = 0;
+
+for (let i = 1; i <= k; i++) {
+  sum += i;
+}
+
+if (sum > n) {
+  console.log(-1);
+} else {
+  n -= sum;
+  if (n % k == 0) console.log(k - 1);
+  else console.log(k);
+}
