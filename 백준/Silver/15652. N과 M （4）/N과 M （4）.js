8b540e67772b82609bb1faba3f93b7c17779b2d9
Honi <87221653+honi31@users.noreply.github.com>
2024-08-10 18:28:23 +0900

    [Silver III] Title: N과 M (4), Time: 172 ms, Memory: 16516 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const n = Number(input[0].split(" ")[0]);
+const m = Number(input[0].split(" ")[1]);
+let arr = [];
+let selected = [];
+for (let i = 1; i <= n; i++) {
+  arr.push(i);
+}
+let answer = "";
+function dfs(arr, depth, start) {
+  let result = [];
+  if (depth === m) {
+    for (let index of selected) result.push(arr[index]);
+    for (let x of result) answer += x + " ";
+    answer += "\n";
+    return;
+  }
+  for (let j = start; j < arr.length; j++) {
+    selected.push(j);
+    dfs(arr, depth + 1, j);
+    selected.pop();
+  }
+}
+
+dfs(arr, 0, 0);
+console.log(answer);
